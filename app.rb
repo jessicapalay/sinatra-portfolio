3511efa7f3f402986cf4d2532a7d4e6fd0f093ea
Jessica Palay <jessica.c.palay@gmail.com>
2013-08-30 15:23:26 -0500

    added email

--- app.rb
+++ app.rb
@@ -1,9 +1,24 @@
 require "sinatra"
+require "pony"
+
+Pony.options = { 
+	:via => :smtp, 
+	:via_options => { 
+ 	 :address        => "smtp.sendgrid.net",
+ 	 :port           => 587,
+ 	 :authentication => :plain,
+ 	 :user_name      => ENV['SENDGRID_USERNAME'], 
+ 	 :password       => ENV['SENDGRID_PASSWORD'],
+ 	 :domain         => "heroku.com",
+ 	 :enable_starttls_auto => true
+	}
+}
 
 get "/" do
 	erb :index
 end
 
 post "/emails" do
-  # send the email
+  Pony.mail(to: "jessica.c.palay@gmail.com", subject: "Message from: #{params[:name]}", from: params[:email], body: params[:message])
+  redirect "/"
 end
